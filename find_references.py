90d481a2da95e7f1ee2c60c6d910048349bffaac
Masiha Ahmadi <esafb52@users.noreply.github.com>
2018-08-11 10:58:09 +0430

    edit and sort reference file

--- find_references.py
+++ find_references.py
@@ -2,12 +2,8 @@ from docx import Document
 
 
 def save_references_file(txt_file_name, lst_txt):
-    if lst_txt is not None:
-        if len(lst_txt) > 1:
-            lst_txt.sort()
-    ls_final = set(lst_txt)
     my_doc = open(txt_file_name, 'w', encoding='utf-8')
-    for p in ls_final:
+    for p in lst_txt:
         my_doc.writelines(str(p).strip() + '\n')
     my_doc.close()
 
@@ -26,21 +22,20 @@ def find_references(path):
                 temp_txt = temp_txt + char
             if char == ')':
                 mode = False
-                if 7 < len(temp_txt) < 100:
+                if 8 < len(temp_txt) < 100:
                     ls_txt_data.append(temp_txt.strip())
-                    print(temp_txt[1:-1])
+                    # print(temp_txt[1:-1])
                 temp_txt = ''
-    return ls_txt_data
+    ls = list(set(ls_txt_data))
+    ls.sort()
+    for item in ls:
+        print(item)
+    return ls
 
 
-def main():
-    docx_file_path = 'g:/qq.docx'
-    result_file_path = 'g:/final_reference.txt'
+if __name__ == '__main__':
+    docx_file_path = 'd:/esa_doc.docx'
+    result_file_path = 'd:/final_reference_esa22.txt'
     lst_ref = find_references(docx_file_path)
     save_references_file(result_file_path, lst_ref)
     print('find references complete  !!!!!!')
-    
-
-if __name__ == '__main__':
-    main()
-   
